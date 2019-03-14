cb4bec88743471a765a643faa8968ac7656c6a48
Charles-Henri Decultot <chdecultot@dokos.io>
2019-03-14 18:13:02 +0100

    Fix: Remove changing fields from stock ledger version

--- erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry_version.py
+++ erpnext/stock/doctype/stock_ledger_entry/stock_ledger_entry_version.py
@@ -12,9 +12,7 @@ def get_data():
 			"version": "1.0.0",
 			"fields": ['item_code', 'serial_no', 'batch_no', 'warehouse', 'posting_date',\
 				'posting_time', 'voucher_type', 'voucher_no', 'voucher_detail_no', 'actual_qty',\
-				'incoming_rate', 'outgoing_rate', 'stock_uom', 'qty_after_transaction',\
-				'valuation_rate', 'stock_value', 'stock_value_difference', 'stock_queue',\
-				'project', 'company', 'fiscal_year'],
+				'incoming_rate', 'outgoing_rate', 'stock_uom', 'project', 'company', 'fiscal_year'],
 			"tables": {},
 			"sanitize": True
 		}
