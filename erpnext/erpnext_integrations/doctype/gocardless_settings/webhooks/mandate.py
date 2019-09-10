0d013d6adba8607b9a4fc19b350be535e67b7828
Charles-Henri Decultot <chdecultot@dokos.io>
2019-09-10 16:21:04 +0200

    feat: GoCardless webhooks

---
+++ erpnext/erpnext_integrations/doctype/gocardless_settings/webhooks/mandate.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Dokos SAS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+import json
+from frappe.utils import nowdate
+
+EVENT_MAP = {
+	'customer_approval_granted': 'change_status',
+	'customer_approval_skipped': 'change_status',
+	'active': 'change_status',
+	'cancelled': 'change_status',
+	'failed': 'change_status',
+	'transferred': 'change_status',
+	'expired': 'change_status',
+	'submitted': 'change_status',
+	'resubmission_requested': 'change_status',
+	'reinstated': 'change_status',
+	'replaced': 'change_status'
+}
+
+STATUS_MAP = {
+	'customer_approval_granted': 'Pending Customer Approval',
+	'customer_approval_skipped': 'Pending Submission',
+	'active': 'Active',
+	'cancelled': 'Cancelled',
+	'failed': 'Failed',
+	'transferred': 'Submitted',
+	'expired': 'Expired',
+	'submitted': 'Submitted',
+	'resubmission_requested': 'Pending Submission',
+	'reinstated': 'Active',
+	'replaced': 'Cancelled'
+}
+
+class GoCardlessMandateWebhookHandler():
+	def __init__(self, **kwargs):
+		self.integration_request = frappe.get_doc(kwargs.get("doctype"), kwargs.get("docname"))
+		self.data = json.loads(self.integration_request.get("data"))
+		self.get_mandates()
+
+		target = EVENT_MAP.get(self.data.get("action"))
+		if not target:
+			self.integration_request.db_set("error", _("This type of event is not handled by ERPNext"))
+			self.integration_request.update_status({}, "Completed")
+
+		else:
+			method = getattr(self, target)
+			method()
+
+		self.add_mandate_to_integration_request(self.mandates[0])
+
+	def create_mandates(self):
+		existing_mandates, non_existing_mandates = self.check_existing_mandates()
+		for mandate in existing_mandates:
+			self.set_status(mandate, STATUS_MAP.get(self.data.get("action")))
+		
+		for mandate in non_existing_mandates:
+			#TODO: Handle mandate creation through API (only for GoCardless Pro accounts)
+			self.integration_request.update_status({}, "Completed")
+
+	def check_existing_mandates(self):
+		if frappe.get_all("GoCardless Mandate", filters={"mandate": ["in", self.mandates]}):
+			existing_list = []
+			non_existing_list = []
+			for mandate in self.mandates:
+				if frappe.db.exists("GoCardless Mandate", dict(mandate=mandate)):
+					existing_list.append(mandate)
+				else:
+					non_existing_list.append(mandate)
+			return existing_list, non_existing_list
+		else:
+			return [], self.mandates
+
+	def change_status(self):
+		for mandate in self.mandates:
+			self.set_status(mandate, STATUS_MAP.get(self.data.get("action")))
+
+	def get_mandates(self):
+		self.mandates = []
+		if isinstance(self.data.get("links"), list):
+			for link in self.data.get("links"):
+				self.mandates.append(link.get("mandate"))
+		else:
+			self.mandates.append(self.data.get("links").get("mandate"))
+
+	def set_status(self, mandate, status):
+		try:
+			frappe.db.set_value("GoCardless Mandate", mandate, "status", status)
+			self.integration_request.update_status({}, "Completed")
+		except Exception as e:
+			self.integration_request.db_set("error", e)
+			self.integration_request.update_status({}, "Failed")
+
+	def add_mandate_to_integration_request(self, mandate):
+		self.integration_request.db_set("reference_doctype", "GoCardless Mandate")
+		self.integration_request.db_set("reference_docname", mandate)
