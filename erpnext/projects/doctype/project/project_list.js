e0a4f5a08ad99e87f2e8beec3d85c7e55ee6a68c
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-09 10:43:29 +0200

    fix: Project list colors and translations

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -2,8 +2,14 @@ frappe.listview_settings['Project'] = {
 	add_fields: ["status", "priority", "is_active", "percent_complete", "expected_end_date", "project_name"],
 	filters:[["status","=", "Open"]],
 	get_indicator: function(doc) {
-		if(doc.status=="Open" && doc.percent_complete) {
+		if (doc.status=="Open" && doc.percent_complete) {
 			return [__("{0}%", [cint(doc.percent_complete)]), "orange", "percent_complete,>,0|status,=,Open"];
+		} else if (doc.status=="Open") {
+			return [__(doc.status), "green", "status,=," + doc.status];
+		} else if (doc.status=="Completed") {
+			return [__(doc.status), "darkgrey", "status,=," + doc.status];
+		} else if (doc.status=="Cancelled") {
+			return [__(doc.status), "grey", "status,=," + doc.status];
 		} else {
 			return [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
 		}
