90d9aaee7882c2c70687d5ced34e0d3f6f12ab56
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-15 18:11:40 +0200

    fix: qty comparison

--- erpnext/accounts/doctype/pricing_rule/utils.py
+++ erpnext/accounts/doctype/pricing_rule/utils.py
@@ -286,8 +286,8 @@ def filter_pricing_rules_for_qty_amount(qty, rate, pricing_rules, args=None):
 		if rule.get("uom"):
 			conversion_factor = get_conversion_factor(rule.item_code, rule.uom).get("conversion_factor", 1)
 
-		if (flt(qty) > (flt(rule.min_qty) * conversion_factor)
-			and (flt(qty) < (rule.max_qty * conversion_factor) if rule.max_qty else True)):
+		if (flt(qty) >= (flt(rule.min_qty) * conversion_factor)
+			and (flt(qty) <= (rule.max_qty * conversion_factor) if rule.max_qty else True)):
 			status = True
 
 		# if user has created item price against the transaction UOM
