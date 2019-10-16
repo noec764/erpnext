1fb9a97f5b2ea3f0c85244c55d97f82b4f643e5c
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-16 14:56:47 +0200

    fix: Wait for frappe to be ready before mounting components

--- erpnext/public/js/portal_payments/portal_payments.js
+++ erpnext/public/js/portal_payments/portal_payments.js
@@ -1,6 +1,8 @@
 import PaymentSelector from './PaymentSelector.vue';
 
-new Vue({
-	el: '#mainview',
-	render: h => h(PaymentSelector)
+frappe.ready(() => {
+	new Vue({
+		el: '#mainview',
+		render: h => h(PaymentSelector)
+	})
 })
