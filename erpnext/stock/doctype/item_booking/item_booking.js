38708c2f99caab5a96216c9ae5ddc84dd130dbbc
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-29 16:21:07 +0100

    fix: Item booking fixes

--- erpnext/stock/doctype/item_booking/item_booking.js
+++ erpnext/stock/doctype/item_booking/item_booking.js
@@ -49,7 +49,9 @@ frappe.ui.form.on('Item Booking', {
 								frappe.datetime.add_minutes(frm.doc.modified, r),
 								frappe.datetime.now_datetime())
 							frm.set_intro()
-							frm.set_intro(__("This document will be automatically deleted in {0} minutes if not validated.", [delay]))
+							if (delay > 0) {
+								frm.set_intro(__("This document will be automatically deleted in {0} minutes if not validated.", [delay]))
+							}
 						}, 10000 )
 					}
 				} )
