bce0af170ab831e80a8d03291762cf93a4c4d49f
Charles-Henri Decultot <chdecultot@dokos.io>
2019-11-08 18:51:11 +0100

    chore: ERPNext update 08/11/2019

--- erpnext/hr/doctype/salary_structure/salary_structure.js
+++ erpnext/hr/doctype/salary_structure/salary_structure.js
@@ -47,9 +47,11 @@ frappe.ui.form.on('Salary Structure', {
 		frm.fields_dict['earnings'].grid.set_column_disp("default_amount", false);
 		frm.fields_dict['deductions'].grid.set_column_disp("default_amount", false);
 
-		frm.add_custom_button(__("Preview Salary Slip"), function() {
-			frm.trigger('preview_salary_slip');
-		});
+		if(frm.doc.docstatus === 1) {
+			frm.add_custom_button(__("Preview Salary Slip"), function() {
+				frm.trigger('preview_salary_slip');
+			});
+		}
 
 		if(frm.doc.docstatus==1) {
 			frm.add_custom_button(__("Assign Salary Structure"), function() {
