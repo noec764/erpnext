80649a3a643ce093fe553afa955ac1a2505bceaf
Charles-Henri Decultot <chdecultot@dokos.io>
2019-12-27 20:03:29 +0100

    fix: Pass delivered qty from Sales order to sales invoice

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -690,6 +690,7 @@ def make_sales_invoice(source_name, target_doc=None, ignore_permissions=False):
 			"field_map": {
 				"name": "so_detail",
 				"parent": "sales_order",
+				"delivered_qty": "delivered_qty"
 			},
 			"postprocess": update_item,
 			"condition": lambda doc: doc.qty and (doc.base_amount==0 or abs(doc.billed_amt) < abs(doc.amount))
