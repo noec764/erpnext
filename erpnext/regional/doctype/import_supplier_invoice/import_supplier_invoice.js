1e0ff470aaff0c389873fb854a83247acb7aa7dc
Charles-Henri Decultot <chdecultot@dokos.io>
2019-12-27 18:04:37 +0100

    feat: Italian einvoicing corrections

--- erpnext/regional/doctype/import_supplier_invoice/import_supplier_invoice.js
+++ erpnext/regional/doctype/import_supplier_invoice/import_supplier_invoice.js
@@ -9,13 +9,38 @@ frappe.ui.form.on('Import Supplier Invoice', {
 				window.setTimeout(title => frm.dashboard.hide_progress(title), 1500, data.title);
 			}
 		});
+	},
+	setup: function(frm) {
+		frm.set_query("tax_account", function(doc) {
+			return {
+				filters: {
+					account_type: 'Tax',
+					company: doc.company
+				}
+			};
+		});
 
-		frm.set_query("tax_account", function() {
-					return {
-						filters: {
-							account_type: 'Tax'
-						}
-					}
-				});
+		frm.set_query("default_buying_price_list", function(doc) {
+			return {
+				filters: {
+					currency: frappe.get_doc(":Company", doc.company).default_currency
+				}
+			};
+		});
+	},
+
+	refresh: function(frm) {
+		frm.trigger("toggle_read_only_fields");
+	},
+
+	toggle_read_only_fields: function(frm) {
+		if (in_list(["File Import Completed", "Processing File Data"], frm.doc.status)) {
+			cur_frm.set_read_only();
+			cur_frm.refresh_fields();
+			frm.set_df_property("import_invoices", "hidden", 1);
+		} else {
+			frm.set_df_property("import_invoices", "hidden", 0);
+		}
 	}
+
 });
