b15ee1ab7843fae38939feb4fdac98c71532d8b7
Charles-Henri Decultot <chdecultot@dokos.io>
2020-01-07 14:18:10 +0100

    fix: GST HSN code in item

--- erpnext/stock/doctype/item/item.js
+++ erpnext/stock/doctype/item/item.js
@@ -139,13 +139,13 @@ frappe.ui.form.on("Item", {
 	},
 
 	gst_hsn_code: function(frm){
-		if(!frm.doc.taxes){
-			frappe.db.get_doc("GST HSN Code", frm.doc.gst_hsn_code).then(hsn_doc=>{
-				frm.doc.taxes = [];
+		if(!frm.doc.taxes.length) {
+			frappe.db.get_doc("GST HSN Code", frm.doc.gst_hsn_code).then(hsn_doc => {
 				$.each(hsn_doc.taxes || [], function(i, tax) {
 					let a = frappe.model.add_child(cur_frm.doc, 'Item Tax', 'taxes');
 					a.item_tax_template = tax.item_tax_template;
 					a.tax_category = tax.tax_category;
+					a.valid_from = tax.valid_from;
 					frm.refresh_field('taxes');
 				});
 			});
