6f68357939cadf77ed508622c14d3b94a6971ba9
Charles-Henri Decultot <chdecultot@dokos.io>
2020-04-08 19:26:54 +0200

    fix(patch): Fix edge cases for Batch

--- erpnext/patches/v12_0/set_total_batch_quantity.py
+++ erpnext/patches/v12_0/set_total_batch_quantity.py
@@ -2,8 +2,10 @@ import frappe
 
 
 def execute():
-    frappe.reload_doc("stock", "doctype", "batch")
+	frappe.reload_doc("stock", "doctype", "batch")
 
 	for batch in frappe.get_all("Batch", fields=["name", "batch_id"]):
-		batch_qty = frappe.db.get_value("Stock Ledger Entry", {"docstatus": 1, "batch_no": batch.batch_id}, "sum(actual_qty)")
+		batch_qty = frappe.db.get_value("Stock Ledger Entry",
+			{"docstatus": 1, "batch_no": batch.batch_id, "is_cancelled": "No"},
+			"sum(actual_qty)") or 0.0
 		frappe.db.set_value("Batch", batch.name, "batch_qty", batch_qty, update_modified=False)
