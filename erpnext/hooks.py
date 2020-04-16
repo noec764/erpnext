61568b788293a958d9ae617f298e50cf650ad741
Charles-Henri Decultot <chdecultot@dokos.io>
2020-04-16 12:52:30 +0200

    fix: Employee loan test case and other fixes

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -305,7 +305,7 @@ scheduler_events = {
 		"erpnext.hr.doctype.leave_ledger_entry.leave_ledger_entry.process_expired_allocation",
 		"erpnext.hr.utils.generate_leave_encashment",
 		"erpnext.projects.doctype.project.project.update_project_sales_billing",
-		"erpnext.loan_management.doctype.loan_security_shortfall.loan_security_shortfall.check_for_ltv_shortfall",
+		"erpnext.loan_management.doctype.loan_security_shortfall.loan_security_shortfall.create_process_loan_security_shortfall",
 		"erpnext.loan_management.doctype.loan_interest_accrual.loan_interest_accrual.make_accrual_interest_entry_for_term_loans"
 	],
 	"monthly_long": [
