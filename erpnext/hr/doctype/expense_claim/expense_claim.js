88352d510625dc52902139adaa0adf8a040885ec
Charles-Henri Decultot <chdecultot@dokos.io>
2020-04-27 19:18:05 +0200

    fix: Expense claims

--- erpnext/hr/doctype/expense_claim/expense_claim.js
+++ erpnext/hr/doctype/expense_claim/expense_claim.js
@@ -167,6 +167,15 @@ frappe.ui.form.on("Expense Claim", {
 			};
 		});
 
+		frm.set_query("cost_center", function() {
+			return {
+				filters: {
+					"company": frm.doc.company,
+					"is_group": 0
+				}
+			};
+		});
+
 		frm.set_query("payable_account", function() {
 			return {
 				filters: {
