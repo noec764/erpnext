40dddcfc367097957734a3e36bd194286bf55172
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-04 11:26:42 +0200

    fix: Party Type filter in payment entry list view

--- erpnext/accounts/doctype/payment_entry/payment_entry_list.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry_list.js
@@ -8,5 +8,14 @@ frappe.listview_settings['Payment Entry'] = {
 		} else if(doc.docstatus == 1 && doc.unreconciled_amount <= 0) {
 			return [__("Reconciled"), "green", "status,==,Reconciled"];
 		}
+	},
+	onload: function(listview) {
+		listview.page.fields_dict.party_type.get_query = function() {
+			return {
+				"filters": {
+					"name": ["in", Object.keys(frappe.boot.party_account_types)],
+				}
+			};
+		};
 	}
 };
