25a676920f0003384d87290f34826326d0893060
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-29 07:30:01 +0200

    fix: only save end date when transactions are returned

--- erpnext/erpnext_integrations/doctype/plaid_settings/plaid_settings.py
+++ erpnext/erpnext_integrations/doctype/plaid_settings/plaid_settings.py
@@ -136,12 +136,12 @@ def sync_transactions(bank, bank_account):
 			result.append(new_bank_transaction(transaction))
 
 		if result:
-			end_date = frappe.db.get_value('Bank Transaction', result.pop(), 'date')
+			last_transaction_date = frappe.db.get_value('Bank Transaction', result.pop(), 'date')
 
 			frappe.logger().info("Plaid added {} new Bank Transactions from '{}' between {} and {}".format(
 				len(result), bank_account, start_date, end_date))
 
-		frappe.db.set_value("Bank Account", bank_account, "last_integration_date", end_date)
+		frappe.db.set_value("Bank Account", bank_account, "last_integration_date", last_transaction_date)
 		return result
 
 	except Exception:
