a34cd786000b80b29b3d7fb325cbc331798eaa4b
Charles-Henri Decultot <chdecultot@dokos.io>
2020-06-22 16:19:00 +0200

    fix: resetting lost reason in opportunity and quotation

--- erpnext/selling/doctype/quotation/quotation.py
+++ erpnext/selling/doctype/quotation/quotation.py
@@ -108,6 +108,8 @@ class Quotation(SellingController):
 		self.update_item_bookings()
 
 	def on_cancel(self):
+		if self.lost_reasons:
+			self.lost_reasons = []
 		super(Quotation, self).on_cancel()
 
 		#update enquiry status
