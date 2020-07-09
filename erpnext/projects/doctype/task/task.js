cb83b78e4d740b7c058294a47d617f3e3ad2aaba
Charles-Henri Decultot <chdecultot@dokos.io>
2020-07-09 09:20:55 +0200

    fix: fetch project-related info in Timesheet

--- erpnext/projects/doctype/task/task.js
+++ erpnext/projects/doctype/task/task.js
@@ -3,15 +3,28 @@
 
 frappe.provide("erpnext.projects");
 
-cur_frm.add_fetch("project", "company", "company");
-
 frappe.ui.form.on("Task", {
-	onload: function(frm) {
-		frm.set_query("task", "depends_on", function() {
-			var filters = {
+	setup: function (frm) {
+		frm.set_query("project", function () {
+			return {
+				query: "erpnext.projects.doctype.task.task.get_project"
+			}
+		});
+
+		frm.make_methods = {
+			'Timesheet': () => frappe.model.open_mapped_doc({
+				method: 'erpnext.projects.doctype.task.task.make_timesheet',
+				frm: frm
+			})
+		}
+
+	},
+	onload: function (frm) {
+		frm.set_query("task", "depends_on", function () {
+			const filters = {
 				name: ["!=", frm.doc.name]
 			};
-			if(frm.doc.project) filters["project"] = frm.doc.project;
+			if (frm.doc.project) filters["project"] = frm.doc.project;
 			return {
 				filters: filters
 			};
@@ -19,31 +32,7 @@ frappe.ui.form.on("Task", {
 	},
 
 	refresh: function(frm) {
-		frm.fields_dict['parent_task'].get_query = function () {
-			return {
-				filters: {
-					"is_group": 1,
-				}
-			}
-		}
-
-		if (!frm.doc.is_group) {
-			if (!frm.is_new()) {
-				if (frappe.model.can_read("Timesheet")) {
-					frm.add_custom_button(__("Timesheet"), () => {
-						frappe.route_options = { "project": frm.doc.project, "task": frm.doc.name }
-						frappe.set_route("List", "Timesheet");
-					}, __("View"), true);
-				}
-
-				if (frappe.model.can_read("Expense Claim")) {
-					frm.add_custom_button(__("Expense Claims"), () => {
-						frappe.route_options = { "project": frm.doc.project, "task": frm.doc.name };
-						frappe.set_route("List", "Expense Claim");
-					}, __("View"), true);
-				}
-			}
-		}
+		frm.set_query("parent_task", { "is_group": 1 });
 	},
 
 	setup: function(frm) {
@@ -69,12 +58,9 @@ frappe.ui.form.on("Task", {
 		})
 	},
 
-	validate: function(frm) {
+	validate: function (frm) {
 		frm.doc.project && frappe.model.remove_from_locals("Project",
 			frm.doc.project);
 	},
 
 });
-
-cur_frm.add_fetch('task', 'subject', 'subject');
-cur_frm.add_fetch('task', 'project', 'project');
