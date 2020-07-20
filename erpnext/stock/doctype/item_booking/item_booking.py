573fcf62460662a43480a3cb61a9429f3d90cfcb
Charles-Henri Decultot <chdecultot@dokos.io>
2020-07-20 14:39:22 +0200

    fix: Better error messages for item booking

--- erpnext/stock/doctype/item_booking/item_booking.py
+++ erpnext/stock/doctype/item_booking/item_booking.py
@@ -681,6 +681,7 @@ def insert_event_in_google_calendar(doc, method=None):
 		doc.db_set("google_calendar_event_id", event.get("id"), update_modified=False)
 		frappe.publish_realtime('event_synced', {"message": _("Event Synced with Google Calendar.")}, user=frappe.session.user)
 	except HttpError as err:
+		frappe.msgprint(f'{_("Google Error")}: {json.loads(err.content)["error"]["message"]}')
 		frappe.throw(_("Google Calendar - Could not insert event in Google Calendar {0}, error code {1}."\
 			).format(account.name, err.resp.status))
 
@@ -718,6 +719,7 @@ def update_event_in_google_calendar(doc, method=None):
 			eventId=doc.google_calendar_event_id, body=event).execute()
 		frappe.publish_realtime('event_synced', {"message": _("Event Synced with Google Calendar.")}, user=frappe.session.user)
 	except HttpError as err:
+		frappe.msgprint(f'{_("Google Error")}: {json.loads(err.content)["error"]["message"]}')
 		frappe.throw(_("Google Calendar - Could not update Event {0} in Google Calendar, error code {1}."\
 			).format(doc.name, err.resp.status))
 
@@ -744,5 +746,6 @@ def delete_event_in_google_calendar(doc, method=None):
 		google_calendar.events().update(calendarId=doc.google_calendar_id, \
 			eventId=doc.google_calendar_event_id, body=event).execute()
 	except HttpError as err:
+		frappe.msgprint(f'{_("Google Error")}: {json.loads(err.content)["error"]["message"]}')
 		frappe.msgprint(_("Google Calendar - Could not delete Event {0} from Google Calendar, error code {1}."\
 			).format(doc.name, err.resp.status))
