7875b4a01fda533229b37078cf42c36cd198f05b
Charles-Henri Decultot <chdecultot@dokos.io>
2020-08-17 08:02:12 +0200

    fix: the JSON object must be str, bytes or bytearray, not list

--- erpnext/accounts/doctype/bank_guarantee/bank_guarantee.py
+++ erpnext/accounts/doctype/bank_guarantee/bank_guarantee.py
@@ -22,9 +22,9 @@ class BankGuarantee(Document):
 			frappe.throw(_("Enter the name of the bank or lending institution before submittting."))
 
 @frappe.whitelist()
-def get_vouchar_detials(column_list, doctype, docname):
+def get_voucher_details(column_list, doctype, docname):
 	column_list = json.loads(column_list)
 	for col in column_list:
 		sanitize_searchfield(col) 
 	return frappe.db.sql(''' select {columns} from `tab{doctype}` where name=%s'''
-		.format(columns=", ".join(json.loads(column_list)), doctype=doctype), docname, as_dict=1)[0]
+		.format(columns=", ".join(column_list), doctype=doctype), docname, as_dict=1)[0]
