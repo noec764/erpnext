fb17caddc780bbfe49070b79f070dea984c82a4a
Charles-Henri Decultot <chdecultot@dokos.io>
2020-08-20 13:23:14 +0200

    fix: incorrect stock value in return case

--- erpnext/controllers/sales_and_purchase_return.py
+++ erpnext/controllers/sales_and_purchase_return.py
@@ -280,6 +280,8 @@ def make_return_doc(doctype, source_name, target_doc=None):
 			target_doc.rejected_warehouse = source_doc.rejected_warehouse
 			target_doc.po_detail = source_doc.po_detail
 			target_doc.pr_detail = source_doc.pr_detail
+			target_doc.purchase_invoice_item = source_doc.name
+
 		elif doctype == "Delivery Note":
 			target_doc.against_sales_order = source_doc.against_sales_order
 			target_doc.against_sales_invoice = source_doc.against_sales_invoice
@@ -287,6 +289,7 @@ def make_return_doc(doctype, source_name, target_doc=None):
 			target_doc.si_detail = source_doc.si_detail
 			target_doc.expense_account = source_doc.expense_account
 			target_doc.dn_detail = source_doc.name
+			target_doc.sales_invoice_item = source_doc.name
 			if default_warehouse_for_sales_return:
 				target_doc.warehouse = default_warehouse_for_sales_return
 		elif doctype == "Sales Invoice" or doctype == "POS Invoice":
