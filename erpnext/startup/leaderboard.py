cc606dd555facf65b7438ad517aa24a184f0cf68
Charles-Henri Decultot <chdecultot@dokos.io>
2020-08-28 14:46:02 +0200

    fix: Leaderboard

--- erpnext/startup/leaderboard.py
+++ erpnext/startup/leaderboard.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, print_function
 import frappe
 from frappe import _
 from frappe.utils import cint
+from frappe.translate import send_translations, get_dict
 
 def get_leaderboards():
 	leaderboards = {
@@ -48,6 +49,9 @@ def get_leaderboards():
 		}
 	}
 
+	leaderboard_file = frappe.get_pymodule_path('erpnext.startup', 'leaderboard.py')
+	send_translations(get_dict("pyfile", leaderboard_file))
+
 	return leaderboards
 
 @frappe.whitelist()
@@ -70,11 +74,11 @@ def get_all_customers(date_range, company, field, limit = None):
 		elif field == "total_qty_sold":
 			select_field = "sum(so_item.stock_qty)"
 
-		date_condition = get_date_condition(date_range, 'so.transaction_date')
+		date_condition = get_date_condition(date_range, 'so.posting_date')
 
 		return frappe.db.sql("""
 			select so.customer as name, {0} as value
-			FROM `tabSales Order` as so JOIN `tabSales Order Item` as so_item
+			FROM `tabSales Invoice` as so JOIN `tabSales Invoice Item` as so_item
 				ON so.name = so_item.parent
 			where so.docstatus = 1 {1} and so.company = %s
 			group by so.customer
@@ -95,18 +99,18 @@ def get_all_items(date_range, company, field, limit = None):
 	else:
 		if field == "total_sales_amount":
 			select_field = "sum(order_item.base_net_amount)"
-			select_doctype = "Sales Order"
+			select_doctype = "Sales Invoice"
 		elif field == "total_purchase_amount":
 			select_field = "sum(order_item.base_net_amount)"
-			select_doctype = "Purchase Order"
+			select_doctype = "Purchase Invoice"
 		elif field == "total_qty_sold":
 			select_field = "sum(order_item.stock_qty)"
-			select_doctype = "Sales Order"
+			select_doctype = "Sales Invoice"
 		elif field == "total_qty_purchased":
 			select_field = "sum(order_item.stock_qty)"
-			select_doctype = "Purchase Order"
+			select_doctype = "Purchase Invoice"
 
-		date_condition = get_date_condition(date_range, 'sales_order.transaction_date')
+		date_condition = get_date_condition(date_range, 'sales_order.posting_date')
 
 		return frappe.db.sql("""
 			select order_item.item_code as name, {0} as value
@@ -124,7 +128,8 @@ def get_all_suppliers(date_range, company, field, limit = None):
 	if field == "outstanding_amount":
 		filters = [['docstatus', '=', '1'], ['company', '=', company]]
 		if date_range:
-			filters.append(['posting_date', 'between', date_range])
+			date_range = frappe.parse_json(date_range)
+			filters.append(['posting_date', 'between', [date_range[0], date_range[1]]])
 		return frappe.db.get_all('Purchase Invoice',
 			fields = ['supplier as name', 'sum(outstanding_amount) as value'],
 			filters = filters,
@@ -142,7 +147,7 @@ def get_all_suppliers(date_range, company, field, limit = None):
 
 		return frappe.db.sql("""
 			select purchase_order.supplier as name, {0} as value
-			FROM `tabPurchase Order` as purchase_order LEFT JOIN `tabPurchase Order Item`
+			FROM `tabPurchase Invoice` as purchase_order LEFT JOIN `tabPurchase Invoice Item`
 				as purchase_order_item ON purchase_order.name = purchase_order_item.parent
 			where
 				purchase_order.docstatus = 1
@@ -166,21 +171,21 @@ def get_all_sales_partner(date_range, company, field, limit = None):
 	}
 	if date_range:
 		date_range = frappe.parse_json(date_range)
-		filters['transaction_date'] = ['between', [date_range[0], date_range[1]]]
+		filters['posting_date'] = ['between', [date_range[0], date_range[1]]]
 
-	return frappe.get_list('Sales Order', fields=[
+	return frappe.get_list('Sales Invoice', fields=[
 		'`sales_partner` as name',
 		'{} as value'.format(select_field),
 	], filters=filters, group_by='sales_partner', order_by='value DESC', limit=limit)
 
 @frappe.whitelist()
 def get_all_sales_person(date_range, company, field = None, limit = 0):
-	date_condition = get_date_condition(date_range, 'sales_order.transaction_date')
+	date_condition = get_date_condition(date_range, 'sales_order.posting_date')
 
 	return frappe.db.sql("""
 		select sales_team.sales_person as name, sum(sales_order.base_net_total) as value
-		from `tabSales Order` as sales_order join `tabSales Team` as sales_team
-			on sales_order.name = sales_team.parent and sales_team.parenttype = 'Sales Order'
+		from `tabSales Invoice` as sales_order join `tabSales Team` as sales_team
+			on sales_order.name = sales_team.parent and sales_team.parenttype = 'Sales Invoice'
 		where sales_order.docstatus = 1
 			and sales_order.company = %s
 			{date_condition}
