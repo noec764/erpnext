2ef405dfd8aa3ab89ad6239d5f3760589e2a538d
Charles-Henri Decultot <chdecultot@dokos.io>
2020-09-18 18:56:35 +0200

    fix: Patches

--- erpnext/patches/v13_0/change_default_pos_print_format.py
+++ erpnext/patches/v13_0/change_default_pos_print_format.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.reload_doc("accounts", "doctype", "POS Profile")
 	frappe.db.sql(
 		"""UPDATE `tabPOS Profile` profile
 		SET profile.`print_format` = 'POS Invoice'
