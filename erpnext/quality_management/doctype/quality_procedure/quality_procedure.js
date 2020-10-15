afcb5076d2c8d0d65205eb32a0059a09eeead7db
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-15 18:11:44 +0200

    fix: quality procedure parent

--- erpnext/quality_management/doctype/quality_procedure/quality_procedure.js
+++ erpnext/quality_management/doctype/quality_procedure/quality_procedure.js
@@ -2,11 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Quality Procedure', {
-    refresh: function(frm) {
+	refresh: function(frm) {
 		frm.set_query("procedure","processes", (frm) =>{
 			return {
 				filters: {
 					name: ["not in", [frm.parent_quality_procedure, frm.name]]
+				}
+			};
+		});
+
+		frm.set_query('parent_quality_procedure', function(){
+			return {
+				filters: {
+					is_group: 1
 				}
 			};
 		});
