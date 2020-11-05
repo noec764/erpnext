21ec6c2ed32fb30165666bc4cdec7523f0ab0e01
Charles-Henri Decultot <chdecultot@dokos.io>
2020-11-05 08:26:26 +0100

    fix: check_booking_credit_rule

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -263,7 +263,7 @@ def get_pricing_rule_for_item(args, price_list_rate=0, doc=None, for_validate=Fa
 
 			if pricing_rule.get('suggestion'): continue
 
-			if pricing_rule.booking_credits_based and doc and not check_booking_credit_rule(args, doc):
+			if pricing_rule.booking_credits_based and not check_booking_credit_rule(args, doc):
 				continue
 
 			item_details.validate_applied_rule = pricing_rule.get("validate_applied_rule", 0)
@@ -457,7 +457,7 @@ def check_booking_credit_rule(args, doc):
 	from erpnext.venue.doctype.booking_credit.booking_credit import get_balance
 	from erpnext.venue.doctype.item_booking.item_booking import get_uom_in_minutes
 
-	if args.get("customer"):
+	if args.get("customer") and doc:
 		balance = get_balance(args.get("customer"), args.get("transaction_date"))
 		convertible_items = frappe.get_all("Booking Credit Conversions",
 			fields=["convertible_item", "booking_credits_item"]
