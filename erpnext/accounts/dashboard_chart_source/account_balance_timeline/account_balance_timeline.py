7504c9c082bafc2e9a14233cd91d7c9f7d3cca6f
Charles-Henri Decultot <chdecultot@dokos.io>
2020-11-25 13:02:09 +0100

    fix: function imports in account_balance_timeline.py

--- erpnext/accounts/dashboard_chart_source/account_balance_timeline/account_balance_timeline.py
+++ erpnext/accounts/dashboard_chart_source/account_balance_timeline/account_balance_timeline.py
@@ -7,8 +7,8 @@ from frappe import _
 from frappe.utils import add_to_date, date_diff, getdate, nowdate, get_last_day, formatdate, get_link_to_form, flt
 from erpnext.accounts.utils import get_currency_precision
 from erpnext.accounts.report.general_ledger.general_ledger import execute
-from frappe.utils.dashboard import cache_source, get_from_date_from_timespan
-from frappe.desk.doctype.dashboard_chart.dashboard_chart import get_period_ending
+from frappe.utils.dashboard import cache_source
+from frappe.utils.dateutils import get_from_date_from_timespan, get_period_ending
 
 from frappe.utils.nestedset import get_descendants_of
 
