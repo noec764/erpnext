824edcd562e43ccb15a5b935a6ed98c9f2f3cc91
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-24 09:08:35 +0100

    fix: leave policy dashboard fix

--- erpnext/hr/doctype/leave_policy/leave_policy_dashboard.py
+++ erpnext/hr/doctype/leave_policy/leave_policy_dashboard.py
@@ -4,14 +4,7 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname':  'leave_policy',
-		'non_standard_fieldnames': {
-			'Employee Grade': 'default_leave_policy'
-		},
 		'transactions': [
-			{
-				'label': _('Employees'),
-				'items': ['Employee', 'Employee Grade']
-			},
 			{
 				'label': _('Leaves'),
 				'items': ['Leave Allocation']
