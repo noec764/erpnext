06de487fd767fea9144556e95bcc064cc0283318
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-09 07:59:11 +0100

    fix: (ui) Empty State text font weight in Item Dashboard

--- erpnext/stock/dashboard/item_dashboard.js
+++ erpnext/stock/dashboard/item_dashboard.js
@@ -135,7 +135,7 @@ erpnext.stock.ItemDashboard = class ItemDashboard {
 			const message = __("No Stock Available Currently");
 			this.content.find('.result').css('text-align', 'center');
 
-			$(`<div class='text-muted' style='margin: 20px 5px; font-weight: lighter;'>
+			$(`<div class='text-muted' style='margin: 20px 5px;'>
 				${message} </div>`).appendTo(this.result);
 		}
 	}
