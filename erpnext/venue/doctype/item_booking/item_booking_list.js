ce09cce97159a7de4d3f77e451b467d3eeecfe93
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-10 16:34:38 +0100

    fix: Filter query for user

--- erpnext/venue/doctype/item_booking/item_booking_list.js
+++ erpnext/venue/doctype/item_booking/item_booking_list.js
@@ -9,5 +9,15 @@ frappe.listview_settings['Item Booking'] = {
 		} else if (doc.status == "Not Confirmed") {
 			return [__("Not Confirmed"), "darkgray", "status,=,Not Confirmed"];
 		}
+	},
+	onload: function(list_view) {
+		if (list_view.page.fields_dict.user) {
+			list_view.page.fields_dict.user.get_query = function() {
+				return {
+					query: "frappe.core.doctype.user.user.user_query",
+					filters: {ignore_user_type: 1}
+				}
+			};
+		}
 	}
 };
