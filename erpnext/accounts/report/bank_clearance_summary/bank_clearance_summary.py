3959c9f13fe9b9f6a05b0a0e0420c38a0a9e63e0
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-11 15:58:53 +0100

    fix: dynamic links for reports

--- erpnext/accounts/report/bank_clearance_summary/bank_clearance_summary.py
+++ erpnext/accounts/report/bank_clearance_summary/bank_clearance_summary.py
@@ -9,23 +9,57 @@ from frappe.utils import nowdate, getdate
 def execute(filters=None):
 	if not filters: filters = {}
 
-	filters["account"] = frappe.db.get_value("Bank Account", filters.bank_account, "account")
-
 	columns = get_columns()
 	data = get_entries(filters)
 
 	return columns, data
 
 def get_columns():
-	return [
-		_("Payment Document") + "::130",
-		_("Payment Entry") + ":Dynamic Link/"+_("Payment Document")+":110",
-		_("Posting Date") + ":Date:100",
-		_("Cheque/Reference No") + "::120",
-		_("Clearance Date") + ":Date:100",
-		_("Against Account") + ":Link/Account:170",
-		_("Amount") + ":Currency:120"
-	]
+	columns = [{
+			"label": _("Payment Document Type"),
+			"fieldname": "payment_document_type",
+			"fieldtype": "Link",
+			"options": "Doctype",
+			"width": 130
+		},
+		{
+			"label": _("Payment Entry"),
+			"fieldname": "payment_entry",
+			"fieldtype": "Dynamic Link",
+			"options": "payment_document_type",
+			"width": 140
+		},
+		{
+			"label": _("Posting Date"),
+			"fieldname": "posting_date",
+			"fieldtype": "Date",
+			"width": 100
+		},
+		{
+			"label": _("Cheque/Reference No"),
+			"fieldname": "cheque_no",
+			"width": 120
+		},
+		{
+			"label": _("Clearance Date"),
+			"fieldname": "clearance_date",
+			"fieldtype": "Date",
+			"width": 100
+		},
+		{
+			"label": _("Against Account"),
+			"fieldname": "against",
+			"fieldtype": "Link",
+			"options": "Account",
+			"width": 170
+		},
+		{
+			"label": _("Amount"),
+			"fieldname": "amount",
+			"width": 120
+		}]
+
+	return columns
 
 def get_conditions(filters):
 	conditions = ""
@@ -51,42 +85,8 @@ def get_entries(filters):
 			if(paid_from=%(account)s, paid_amount * -1, received_amount)
 		FROM 
 			`tabPayment Entry`
-		WHERE
+		WHERE 
 			docstatus=1 and (paid_from = %(account)s or paid_to = %(account)s) {0}
 			order by posting_date DESC, name DESC""".format(conditions), filters, as_list=1)
 
-	purchase_invoices =  frappe.db.sql("""SELECT
-			"Purchase Invoice", name, posting_date, remarks, clearance_date, supplier,
-			base_paid_amount * -1
-		FROM
-			`tabPurchase Invoice`
-		WHERE
-			docstatus=1 and is_paid=1 and cash_bank_account=%(account)s {0}
-			order by posting_date DESC, name DESC""".format(conditions), filters, as_list=1)
-
-	sales_invoices =  frappe.db.sql("""SELECT
-			"Sales Invoice", si.name, si.posting_date, si.remarks, sip.clearance_date, si.customer,
-			sip.base_amount
-		FROM
-			`tabSales Invoice Payment` sip
-		LEFT JOIN
-			`tabSales Invoice` si
-		ON
-			si.name = sip.parent
-		WHERE
-			si.docstatus=1 and si.is_pos=1 and sip.account=%(account)s {0}
-			order by si.posting_date DESC, si.name DESC""".format(conditions), filters, as_list=1)
-
-	mops = tuple([frappe.db.escape(x.parent) for x in frappe.get_all("Mode of Payment Account", {"default_account": filters.get("account")}, "parent")])
-	if not mops:
-		frappe.throw(_("Please create at least one mode of payment for this bank account first"))
-	expense_claims =  frappe.db.sql("""SELECT
-			"Expense Claim", name, posting_date, remark, clearance_date, employee,
-			total_amount_reimbursed
-		FROM
-			`tabExpense Claim`
-		WHERE
-			docstatus=1 and mode_of_payment in ({1}) {0}
-			order by posting_date DESC, name DESC""".format(conditions, ", ".join(mops)), filters, as_list=1)
-
-	return sorted(journal_entries + payment_entries + purchase_invoices + sales_invoices, key=lambda k: k[2] or getdate(nowdate()))
+	return sorted(journal_entries + payment_entries, key=lambda k: k[2] or getdate(nowdate()))
