6849d5abfaa6924f647083d86a999ccdd65f3ede
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-11 18:55:39 +0100

    fix: Indent error

--- erpnext/accounts/doctype/pos_invoice/pos_invoice.py
+++ erpnext/accounts/doctype/pos_invoice/pos_invoice.py
@@ -410,21 +410,21 @@ def get_new_payment_request(self, mop):
 		}
 		return make_payment_request(**args)
 
-	def get_existing_payment_request(self, pay):
-		payment_gateway_account = frappe.db.get_value("Payment Gateway Account", {
-			"payment_account": pay.account,
-		}, ["name"])
+def get_existing_payment_request(self, pay):
+	payment_gateway_account = frappe.db.get_value("Payment Gateway Account", {
+		"payment_account": pay.account,
+	}, ["name"])
 
-		args = {
-			'doctype': 'Payment Request',
-			'reference_doctype': 'POS Invoice',
-			'reference_name': self.name,
-			'payment_gateway_account': payment_gateway_account,
-			'email_to': self.contact_mobile
-		}
-		pr = frappe.db.exists(args)
-		if pr:
-			return frappe.get_doc('Payment Request', pr[0][0])
+	args = {
+		'doctype': 'Payment Request',
+		'reference_doctype': 'POS Invoice',
+		'reference_name': self.name,
+		'payment_gateway_account': payment_gateway_account,
+		'email_to': self.contact_mobile
+	}
+	pr = frappe.db.exists(args)
+	if pr:
+		return frappe.get_doc('Payment Request', pr[0][0])
 
 @frappe.whitelist()
 def get_stock_availability(item_code, warehouse):
