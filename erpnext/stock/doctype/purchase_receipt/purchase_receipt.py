9265097bbd28ef8dcb966ad35dada4fc0275b2c1
Charles-Henri <chdecultot@dokos.io>
2021-07-30 13:48:11 +0200

    fix: empty "against account" in Purchase Receipt GLE

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -438,7 +438,7 @@ class PurchaseReceipt(BuyingController):
 			"cost_center": cost_center,
 			"debit": debit,
 			"credit": credit,
-			"against_account": against_account,
+			"against": against_account,
 			"remarks": remarks,
 		}
 
