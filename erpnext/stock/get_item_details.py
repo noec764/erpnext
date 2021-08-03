f9c278e80fe41c2f37646f4dc7134c23bd5f10a2
Charles-Henri <chdecultot@dokos.io>
2021-08-03 19:50:36 +0200

    fix: Reset weight_per_unit on replacing Item

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -310,8 +310,8 @@ def get_basic_details(args, item, overwrite_warehouse=True):
 		"transaction_date": args.get("transaction_date"),
 		"against_blanket_order": args.get("against_blanket_order"),
 		"bom_no": item.get("default_bom"),
-		"weight_per_unit": args.get("weight_per_unit") or item.get("weight_per_unit"),
-		"weight_uom": args.get("weight_uom") or item.get("weight_uom"),
+		"weight_per_unit": item.get("weight_per_unit"),
+		"weight_uom": item.get("weight_uom")
 		"is_down_payment_item": item.get("is_down_payment_item"),
 		"down_payment_rate": item.get("down_payment_percentage")
 	})
