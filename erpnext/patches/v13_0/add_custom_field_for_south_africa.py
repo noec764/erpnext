b1d69817dce30b11af89e01b3961bb159d37633b
Charles-Henri <chdecultot@dokos.io>
2021-09-04 15:42:08 +0200

    fix: south africa vat patch failure

--- erpnext/patches/v13_0/add_custom_field_for_south_africa.py
+++ erpnext/patches/v13_0/add_custom_field_for_south_africa.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2020, Frappe and Contributors
 # License: GNU General Public License v3. See license.txt
 
-from __future__ import unicode_literals
 import frappe
 from erpnext.regional.south_africa.setup import make_custom_fields, add_permissions
 
@@ -9,6 +8,9 @@ def execute():
 	company = frappe.get_all('Company', filters = {'country': 'South Africa'})
 	if not company:
 		return
+
+	frappe.reload_doc('regional', 'doctype', 'south_africa_vat_settings')
+	frappe.reload_doc('accounts', 'doctype', 'south_africa_vat_account')
 
 	make_custom_fields()
 	add_permissions()
