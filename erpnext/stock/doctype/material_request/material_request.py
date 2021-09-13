26953510408cc7f7275641b4f4d199f2c35c1c2c
Charles-Henri <chdecultot@dokos.io>
2021-09-13 21:45:49 +0200

    feat: (get_items_from) filter material request item in purchase order

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+
 import json
 
 from frappe.utils import cstr, flt, getdate, new_line_sep, nowdate, add_days, get_link_to_form
@@ -269,7 +270,10 @@ def update_status(name, status):
 	material_request.update_status(status)
 
 @frappe.whitelist()
-def make_purchase_order(source_name, target_doc=None):
+def make_purchase_order(source_name, target_doc=None, args={}):
+
+	if isinstance(args, str):
+		args = json.loads(args)
 
 	def postprocess(source, target_doc):
 		if frappe.flags.args and frappe.flags.args.default_supplier:
@@ -284,7 +288,10 @@ def make_purchase_order(source_name, target_doc=None):
 		set_missing_values(source, target_doc)
 
 	def select_item(d):
-		return d.ordered_qty < d.stock_qty
+		filtered_items = args.get('filtered_children', [])
+		child_filter = d.name in filtered_items if filtered_items else True
+
+		return d.ordered_qty < d.stock_qty and child_filter
 
 	doclist = get_mapped_doc("Material Request", source_name, 	{
 		"Material Request": {
