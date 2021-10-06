a022a420d9ca6d14c925d9d9fe5e4d2a81183e07
Charles-Henri <chdecultot@dokos.io>
2021-10-06 16:55:48 +0200

    fix: missing link in dashboard missing on SI from DN

--- erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
@@ -5,7 +5,6 @@ def get_data():
 	return {
 		'fieldname': 'sales_invoice',
 		'non_standard_fieldnames': {
-			'Delivery Note': 'against_sales_invoice',
 			'Journal Entry': 'reference_name',
 			'Payment Entry': 'reference_name',
 			'Payment Request': 'reference_name',
@@ -14,7 +13,8 @@ def get_data():
 			'Bank Transaction': 'payment_entry'
 		},
 		'internal_links': {
-			'Sales Order': ['items', 'sales_order']
+			'Sales Order': ['items', 'sales_order'],
+			'Delivery Note': ['items', 'delivery_note'],
 		},
 		'transactions': [
 			{
