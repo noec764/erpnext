87434f546e6d12abc8aec596bb1eeb365cdb3d94
Charles-Henri <chdecultot@dokos.io>
2021-10-30 23:11:21 +0200

    fix: Make status filter optional

--- erpnext/assets/report/fixed_asset_register/fixed_asset_register.py
+++ erpnext/assets/report/fixed_asset_register/fixed_asset_register.py
@@ -38,12 +38,13 @@ def get_conditions(filters):
 	if filters.get('cost_center'):
 		conditions["cost_center"] = filters.get('cost_center')
 
-	# In Store assets are those that are not sold or scrapped
-	operand = 'not in'
-	if status not in 'In Location':
-		operand = 'in'
+	if status:
+		# In Store assets are those that are not sold or scrapped
+		operand = 'not in'
+		if status not in 'In Location':
+			operand = 'in'
 
-	conditions['status'] = (operand, ['Sold', 'Scrapped'])
+		conditions['status'] = (operand, ['Sold', 'Scrapped'])
 
 	return conditions
 
@@ -100,7 +101,7 @@ def prepare_chart_data(data, filters):
 	labels_values_map = {}
 	date_field = frappe.scrub(filters.date_based_on)
 
-	period_list = get_period_list(filters.from_fiscal_year, filters.to_fiscal_year, 
+	period_list = get_period_list(filters.from_fiscal_year, filters.to_fiscal_year,
 		filters.from_date, filters.to_date, filters.filter_based_on, "Monthly", company=filters.company)
 
 	for d in period_list:
