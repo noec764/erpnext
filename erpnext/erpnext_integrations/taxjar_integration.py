17b72a72dc94ddc0b75120da6f5d9f97f728fc74
Charles-Henri <chdecultot@dokos.io>
2021-11-08 22:31:20 +0100

    fix: Taxjar customer_address fix, currency fix

--- erpnext/erpnext_integrations/taxjar_integration.py
+++ erpnext/erpnext_integrations/taxjar_integration.py
@@ -6,7 +6,7 @@ from frappe import _
 from frappe.contacts.doctype.address.address import get_company_address
 from frappe.utils import cint, flt
 
-from erpnext import get_default_company
+from erpnext import get_default_company, get_region
 
 TAX_ACCOUNT_HEAD = frappe.db.get_single_value("TaxJar Settings", "tax_account_head")
 SHIP_ACCOUNT_HEAD = frappe.db.get_single_value("TaxJar Settings", "shipping_account_head")
@@ -159,8 +159,7 @@ def set_sales_tax(doc, method):
 	if not TAXJAR_CALCULATE_TAX:
 		return
 
-	taxjar_settings_company = frappe.db.get_single_value('TaxJar Settings','current_company')
-	if taxjar_settings_company not in USA_COMPANIES:
+	if get_region(doc.company) != 'United States':
 		return
 
 	if not doc.items:
