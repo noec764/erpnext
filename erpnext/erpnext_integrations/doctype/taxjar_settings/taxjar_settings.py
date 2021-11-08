e8aca8000b840cd45076165a9d68724074a31fc6
Charles-Henri <chdecultot@dokos.io>
2021-11-08 22:36:23 +0100

    fix: Taxjar integration

--- erpnext/erpnext_integrations/doctype/taxjar_settings/taxjar_settings.py
+++ erpnext/erpnext_integrations/doctype/taxjar_settings/taxjar_settings.py
@@ -1,7 +1,5 @@
-
 # Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
-
 
 
 import json
--- erpnext/erpnext_integrations/taxjar_integration.py
+++ erpnext/erpnext_integrations/taxjar_integration.py
@@ -19,7 +19,7 @@ SUPPORTED_STATE_CODES = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', '
 	'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE',
 	'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD',
 	'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-USA_COMPANIES = frappe.get_all('Company', filters = {'country': 'United States'}, fields=['name'], pluck='name')
+
 
 
 def get_client():
