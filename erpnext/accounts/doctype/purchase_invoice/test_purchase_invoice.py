a6db076a92f4e94f7a7baa653c0cf243b87c656b
Charles-Henri <chdecultot@dokos.io>
2021-11-17 18:51:49 +0100

    fix: POS idx issue in taxes table while merging

--- erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
+++ erpnext/accounts/doctype/purchase_invoice/test_purchase_invoice.py
@@ -11,6 +11,7 @@ from frappe.utils import cint, flt, today, nowdate, add_days, getdate
 import frappe.defaults
 from erpnext.stock.doctype.purchase_receipt.test_purchase_receipt import make_purchase_receipt, get_taxes
 from erpnext.controllers.accounts_controller import get_payment_terms
+from erpnext.controllers.buying_controller import QtyMismatchError
 from erpnext.exceptions import InvalidCurrency
 from erpnext.stock.doctype.stock_entry.test_stock_entry import get_qty_after_transaction
 from erpnext.projects.doctype.project.test_project import make_project
