c46eddcf1962a8bcc535695d3354fdf0912086b8
Charles-Henri <chdecultot@dokos.io>
2021-11-24 15:19:06 +0100

    fix: Item booking calendar

--- erpnext/public/js/calendar_pages/item_booking_calendar.js
+++ erpnext/public/js/calendar_pages/item_booking_calendar.js
@@ -84,6 +84,7 @@ class ItemCalendar {
 			eventClassNames: 'item-booking-calendar',
 			initialView: me.get_initial_display_view(),
 			headerToolbar: me.get_header_toolbar(),
+			contentHeight: "auto",
 			weekends: true,
 			buttonText: {
 				today: __("Today"),
@@ -109,9 +110,6 @@ class ItemCalendar {
 			defaultDate: this.getDefaultDate,
 			eventClick: function(event) {
 				me.eventClick(event)
-			},
-			datesSet: function(event) {
-				return me.datesSet(event);
 			},
 			displayEventTime: this.get_time_display(),
 			allDayContent: function() {
@@ -146,11 +144,5 @@ class ItemCalendar {
 				this.refresh();
 			})
 		});
-	}
-
-	datesSet(event) {
-		if (event.view.activeStart && this.parent.calendar_type !== "Monthly") {
-			this.fullCalendar.gotoDate(event.view.activeStart)
-		}
 	}
 }
