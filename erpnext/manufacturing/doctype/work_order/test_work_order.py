04aa866b2e510707e40418f6e0d29e6941a55aec
Charles-Henri <chdecultot@dokos.io>
2021-12-02 18:54:57 +0100

    test: timeout certain tests in work order to avoid stuck tests

--- erpnext/manufacturing/doctype/work_order/test_work_order.py
+++ erpnext/manufacturing/doctype/work_order/test_work_order.py
@@ -21,9 +21,10 @@ from erpnext.stock.doctype.item.test_item import create_item, make_item
 from erpnext.stock.doctype.stock_entry import test_stock_entry
 from erpnext.stock.doctype.warehouse.test_warehouse import create_warehouse
 from erpnext.stock.utils import get_bin
+from erpnext.tests.utils import ERPNextTestCase, timeout
 
 
-class TestWorkOrder(unittest.TestCase):
+class TestWorkOrder(ERPNextTestCase):
 	def setUp(self):
 		self.warehouse = '_Test Warehouse 2 - _TC'
 		self.item = '_Test Item'
