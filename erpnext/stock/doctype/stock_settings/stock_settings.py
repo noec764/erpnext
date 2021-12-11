b56e47f126e30a4a0efd2a2cf2b644113d64b40a
Charles-Henri <chdecultot@dokos.io>
2021-12-11 17:22:42 +0100

    fix: validate pending reposts before freezing stock/account

--- erpnext/stock/doctype/stock_settings/stock_settings.py
+++ erpnext/stock/doctype/stock_settings/stock_settings.py
@@ -3,13 +3,14 @@
 
 # For license information, please see license.txt
 
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.utils.html_utils import clean_html
 from frappe.utils import cint
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+
+from erpnext.stock.utils import check_pending_reposting
 
 class StockSettings(Document):
 	def validate(self):
@@ -35,6 +36,7 @@ class StockSettings(Document):
 		self.validate_warehouses()
 		self.cant_change_valuation_method()
 		self.validate_clean_description_html()
+		self.validate_pending_reposts()
 
 	def validate_warehouses(self):
 		warehouse_fields = ["default_warehouse", "sample_retention_warehouse"]
@@ -62,6 +64,10 @@ class StockSettings(Document):
 			and not int(self.db_get('clean_description_html') or 0):
 			# changed to text
 			frappe.enqueue('erpnext.stock.doctype.stock_settings.stock_settings.clean_all_descriptions', now=frappe.flags.in_test)
+
+	def validate_pending_reposts(self):
+		if self.stock_frozen_upto:
+			check_pending_reposting(self.stock_frozen_upto)
 
 	def on_update(self):
 		self.toggle_warehouse_field_for_inter_warehouse_transfer()
