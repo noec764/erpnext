e64b6831647daf473975a34d1659d1b8b9298d32
Charles-Henri <chdecultot@dokos.io>
2022-02-28 09:35:14 +0100

    fix: Regional print format addition

--- erpnext/regional/india/setup.py
+++ erpnext/regional/india/setup.py
@@ -115,7 +115,7 @@ def add_permissions():
 
 def add_print_formats():
 	frappe.reload_doc("regional", "print_format", "gst_tax_invoice")
-	frappe.reload_doc("accounts", "print_format", "gst_pos_invoice")
+	frappe.reload_doc("selling", "print_format", "gst_pos_invoice")
 
 	frappe.db.set_value("Print Format", "GST POS Invoice", "disabled", 0)
 	frappe.db.set_value("Print Format", "GST Tax Invoice", "disabled", 0)
