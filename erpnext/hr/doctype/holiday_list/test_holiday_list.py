ec333123c477817f2ee3952606851255dfbf86a3
Charles-Henri <chdecultot@dokos.io>
2022-03-14 09:10:51 +0100

    refactor: Employee Leave Balance

--- erpnext/hr/doctype/holiday_list/test_holiday_list.py
+++ erpnext/hr/doctype/holiday_list/test_holiday_list.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
-
-import frappe
 import unittest
-from frappe.utils import getdate
+from contextlib import contextmanager
 from datetime import timedelta
 
+import frappe
+from frappe.utils import getdate
 
 class TestHolidayList(unittest.TestCase):
 	def test_holiday_list(self):
@@ -30,3 +30,23 @@ def make_holiday_list(name, from_date=getdate()-timedelta(days=10), to_date=getd
 		"holidays" : holiday_dates
 		}).insert()
 	return doc
+
+@contextmanager
+def set_holiday_list(holiday_list, company_name):
+	"""
+	Context manager for setting holiday list in tests
+	"""
+	try:
+		company = frappe.get_doc('Company', company_name)
+		previous_holiday_list = company.default_holiday_list
+
+		company.default_holiday_list = holiday_list
+		company.save()
+
+		yield
+
+	finally:
+		# restore holiday list setup
+		company = frappe.get_doc('Company', company_name)
+		company.default_holiday_list = previous_holiday_list
+		company.save()
