d5ad8ec257504fbc3e1e66f89420acbb2b8c61ea
Charles-Henri <chdecultot@dokos.io>
2022-03-18 08:10:26 +0100

    test: fix holiday list creation causing flaky tests

--- erpnext/hr/doctype/leave_application/test_leave_application.py
+++ erpnext/hr/doctype/leave_application/test_leave_application.py
@@ -82,7 +82,11 @@ class TestLeaveApplication(unittest.TestCase):
 		set_leave_approver()
 
 		frappe.db.delete("Attendance", {"employee": "_T-Employee-00001"})
-		self.holiday_list = make_holiday_list()
+		frappe.db.set_value("Employee", "_T-Employee-00001", "holiday_list", "")
+
+		from_date = get_year_start(getdate())
+		to_date = get_year_ending(getdate())
+		self.holiday_list = make_holiday_list(from_date=from_date, to_date=to_date)
 
 		if not frappe.db.exists("Leave Type", "_Test Leave Type"):
 			frappe.get_doc(dict(
@@ -276,6 +280,7 @@ class TestLeaveApplication(unittest.TestCase):
 
 		leave_application = make_leave_application(employee.name, first_sunday, add_days(first_sunday, 3), leave_type.name)
 		leave_application.reload()
+
 		self.assertEqual(leave_application.total_leave_days, 4)
 		self.assertEqual(frappe.db.count('Attendance', {'leave_application': leave_application.name}), 4)
 
--- erpnext/payroll/doctype/salary_slip/test_salary_slip.py
+++ erpnext/payroll/doctype/salary_slip/test_salary_slip.py
@@ -343,7 +343,7 @@ class TestSalarySlip(unittest.TestCase):
 
 		frappe.db.set_value("Payroll Settings", None, "email_salary_slip_to_employee", 1)
 
-		make_employee("test_email_salary_slip@salary.com")
+		make_employee("test_email_salary_slip@salary.com", company="_Test Company")
 		ss = make_employee_salary_slip("test_email_salary_slip@salary.com", "Monthly", "Test Salary Slip Email")
 		ss.company = "_Test Company"
 		ss.save()
@@ -1023,18 +1023,19 @@ def setup_test():
 def make_holiday_list(list_name=None, from_date=None, to_date=None):
 	fiscal_year = get_fiscal_year(nowdate(), company=erpnext.get_default_company())
 	name = list_name or "Salary Slip Test Holiday List"
-	holiday_list = frappe.db.exists("Holiday List", name)
-	if not holiday_list:
-		holiday_list = frappe.get_doc({
-			"doctype": "Holiday List",
-			"holiday_list_name": name,
-			"from_date": from_date or fiscal_year[1],
-			"to_date": to_date or fiscal_year[2],
-			"weekly_off": "Sunday"
-		}).insert()
-		holiday_list.get_weekly_off_dates()
-		holiday_list.save()
-		holiday_list = holiday_list.name
+
+	frappe.delete_doc_if_exists("Holiday List", name, force=True)
+
+	holiday_list = frappe.get_doc({
+		"doctype": "Holiday List",
+		"holiday_list_name": name,
+		"from_date": from_date or fiscal_year[1],
+		"to_date": to_date or fiscal_year[2],
+		"weekly_off": "Sunday"
+	}).insert()
+	holiday_list.get_weekly_off_dates()
+	holiday_list.save()
+	holiday_list = holiday_list.name
 
 	return holiday_list
 
