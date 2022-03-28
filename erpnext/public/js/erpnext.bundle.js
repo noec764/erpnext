baf4ee93bef44b547827e6b086da071701b8a97b
Charles-Henri <chdecultot@dokos.io>
2022-03-28 15:05:36 +0200

    refactor: transaction.js -> barcode_scanner.js

--- erpnext/public/js/erpnext.bundle.js
+++ erpnext/public/js/erpnext.bundle.js
@@ -17,6 +17,7 @@ import "./utils/customer_quick_entry";
 import "./utils/supplier_quick_entry";
 import "./call_popup/call_popup";
 import "./utils/dimension_tree_filter";
+import "./utils/barcode_scanner";
 import "./telephony";
 import "./templates/call_link.html";
 import "./bulk_transaction_processing";
