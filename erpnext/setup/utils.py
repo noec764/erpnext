6cf0248e4e51dde7903fb088dc3264065dc2ab5e
Charles-Henri <chdecultot@dokos.io>
2022-03-28 09:50:44 +0200

    refactor: replace dead duplicate code

--- erpnext/setup/utils.py
+++ erpnext/setup/utils.py
@@ -148,18 +148,9 @@ def set_defaults_for_tests():
 	frappe.db.set_single_value("Stock Settings", "auto_insert_price_list_rate_if_missing", 0)
 
 def insert_record(records):
-	for r in records:
-		doc = frappe.new_doc(r.get("doctype"))
-		doc.update(r)
-		try:
-			doc.insert(ignore_permissions=True)
-		except frappe.DuplicateEntryError as e:
-			# pass DuplicateEntryError and continue
-			if e.args and e.args[0]==doc.doctype and e.args[1]==doc.name:
-				# make sure DuplicateEntryError is for the exact same doc and not a related doc
-				pass
-			else:
-				raise
+	from frappe.desk.page.setup_wizard.setup_wizard import make_records
+
+	make_records(records)
 
 def welcome_email():
 	site_name = get_default_company() or "Dokos"
