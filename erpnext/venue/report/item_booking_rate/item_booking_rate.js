83f6ac7012d6522f5c0cb2dcec1f6aa3d42e2367
Charles-Henri <chdecultot@dokos.io>
2022-05-27 15:45:42 +0200

    refactir Item booking rate report

--- erpnext/venue/report/item_booking_rate/item_booking_rate.js
+++ erpnext/venue/report/item_booking_rate/item_booking_rate.js
@@ -5,16 +5,12 @@
 frappe.query_reports["Item Booking Rate"] = {
 	filters: [
 		{
-			fieldname: "period",
-			label: __("Period"),
-			fieldtype: "Select",
-			options: [
-				{ "value": "Monthly", "label": __("Monthly") },
-				{ "value": "Quarterly", "label": __("Quarterly") },
-				{ "value": "Half-Yearly", "label": __("Half-Yearly") },
-				{ "value": "Yearly", "label": __("Yearly") }
-			],
-			default: "Monthly"
+			fieldtype: 'Link',
+			label: __('Company'),
+			fieldname: 'company',
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1
 		},
 		{
 			fieldtype: 'DateRange',
@@ -28,7 +24,16 @@ frappe.query_reports["Item Booking Rate"] = {
 			label: __('Status'),
 			fieldname: 'status',
 			default: "Confirmed",
-			options: ["Confirmed", "Confirmed and not confirmed"],
+			options: [
+				{
+					"value": "Confirmed",
+					"label": __("Confirmed")
+				},
+				{
+					"value": "Confirmed and not confirmed",
+					"label": __("Confirmed and not confirmed")
+				}
+			],
 			reqd: 1
 		}
 	]
--- erpnext/venue/report/item_booking_rate/item_booking_rate.py
+++ erpnext/venue/report/item_booking_rate/item_booking_rate.py
@@ -1,32 +1,23 @@
 # Copyright (c) 2020, Dokos SAS and contributors
 # For license information, please see license.txt
 
-
-import calendar
-import datetime
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 import frappe
 from frappe import _
-from frappe.utils import flt, formatdate, getdate
+from frappe.utils import flt
+from frappe.utils.dateutils import get_dates_from_timegrain
 
-from erpnext.accounts.dashboard_chart_source.account_balance_timeline.account_balance_timeline import (
-	get_dates_from_timegrain,
-)
 from erpnext.venue.doctype.item_booking.item_booking import get_item_calendar
 
 
 def execute(filters=None):
-	dates = get_dates_from_timegrain(
-		filters.get("date_range")[0], filters.get("date_range")[1], filters.get("period")
-	)
-	data, chart = get_data(filters, dates)
-	columns = get_columns(filters, dates)
+	data, chart = get_data(filters)
+	columns = get_columns()
 	return columns, data, [], chart
 
 
-def get_data(filters, dates):
-
+def get_data(filters):
 	if not filters.get("date_range"):
 		return []
 
@@ -46,101 +37,75 @@ def get_data(filters, dates):
 	)
 
 	items_dict = defaultdict(lambda: defaultdict(float))
-	sorted_dates = sorted(dates)
 
 	# Get minutes booked
 	for ib in item_booking:
 		ib["diff_minutes"] = (ib.get("ends_on") - ib.get("starts_on")).total_seconds() / 60.0
-		closest = None
-
-		for date in sorted_dates:
-			if closest is None and getdate(ib.get("starts_on")) <= getdate(date):
-				closest = date
 
 		items_dict[ib["item"]]["item_name"] = ib["item_name"]
-		items_dict[ib["item"]][frappe.scrub(str(closest))] += flt(ib["diff_minutes"]) / 60.0
 		items_dict[ib["item"]]["total"] += flt(ib["diff_minutes"]) / 60.0
+		items_dict[ib["item"]]["capacity"] = 0.0
 
-	get_calendar_capacity(filters, items_dict, sorted_dates)
+	get_calendar_capacity(filters, items_dict)
 
 	output = [{"item": x, **items_dict[x]} for x in items_dict]
-
-	for date in dates:
-		output = [
-			{
-				f"{frappe.scrub(str(date))}_percent": (
-					flt(x.get(frappe.scrub(str(date)))) / flt(x[f"{frappe.scrub(str(date))}_capacity"] or 1) * 100
-				),
-				**x,
-			}
-			for x in output
-		]
-
-	scrubed_dates = [frappe.scrub(str(date)) for date in dates]
+	print(output)
 
 	for line in output:
-		line["capacity"] = sum([line.get(x) for x in line if x.endswith("_capacity")])
+		line["percent"] = (
+			flt(line["total"]) / flt(line["capacity"]) if line["capacity"] else 1.0
+		) * 100.0
 
 	chart_data = get_chart_data(output)
 
 	return output, chart_data
 
 
-def get_calendar_capacity(filters, items, dates):
+def get_calendar_capacity(filters, items):
+	holiday_list = frappe.get_cached_value("Company", filters.get("company"), "default_holiday_list")
+	holidays = frappe.get_all(
+		"Holiday",
+		filters={
+			"parent": holiday_list,
+			"holiday_date": ("between", [filters.get("date_range")[0], filters.get("date_range")[1]]),
+		},
+		pluck="holiday_date",
+	)
+
 	for item in items:
 		daily_capacity = defaultdict(float)
 		calendar = get_item_calendar(item)
+
 		for line in calendar.get("calendar"):
 			daily_capacity[line.day] += (
 				line.get("end_time") - line.get("start_time")
 			).total_seconds() / 3600
 
-		prev_date = filters.get("date_range")[0]
-		for date in dates:
-			next_date = min(getdate(date), getdate(filters.get("date_range")[1]))
-			weekdays = dict(count_weekday(prev_date, next_date))
-			items[item][f"{frappe.scrub(str(date))}_capacity"] = sum(
-				[daily_capacity[x] * weekdays[x[:3]] for x in daily_capacity if x[:3] in weekdays]
-			)
-			prev_date = date
+		for index, date in enumerate(filters.get("date_range")):
+			for sub_date in get_dates_from_timegrain(
+				filters.get("date_range")[index - 1] if index > 0 else filters.get("date_range")[0], date
+			):
+				if sub_date not in holidays:
+					items[item]["capacity"] += flt(daily_capacity.get(sub_date.strftime("%A")))
 
 
-def dates_between(start, end):
-	while start <= end:
-		yield start
-		start += datetime.timedelta(1)
-
-
-def count_weekday(start, end):
-	counter = Counter()
-	for date in dates_between(getdate(start), getdate(end)):
-		counter[date.strftime("%a")] += 1
-	return counter
-
-
-def get_columns(filters, dates):
+def get_columns():
 	columns = [
 		{"label": _("Item"), "fieldtype": "Link", "fieldname": "item", "options": "Item", "width": 180},
 		{"label": _("Item Name"), "fieldtype": "Data", "fieldname": "item_name", "width": 250},
+		{
+			"label": _("Hours Booked"),
+			"fieldtype": "Int",
+			"fieldname": "total",
+			"width": 250,
+		},
+		{
+			"label": _("Booking rate"),
+			"fieldtype": "Percent",
+			"fieldname": "percent",
+			"width": 250,
+		},
 	]
-
-	for date in dates:
-		columns.extend(
-			[
-				{
-					"label": _("Hours Booked") + " - " + formatdate(date),
-					"fieldtype": "Int",
-					"fieldname": frappe.scrub(str(date)),
-					"width": 250,
-				},
-				{
-					"label": _("Booking rate") + " - " + formatdate(date),
-					"fieldtype": "Percent",
-					"fieldname": frappe.scrub(str(date)) + "_percent",
-					"width": 250,
-				},
-			]
-		)
 
 	return columns
 
