d93b9ec42252d226b46a2632f097875ef917e1fb
Charles-Henri <chdecultot@dokos.io>
2022-07-21 11:22:47 +0200

    fix: Move HR specific regional elements to HRMS

--- erpnext/regional/france/setup.py
+++ erpnext/regional/france/setup.py
@@ -1,16 +1,13 @@
 # Copyright (c) 2022, Dokos SAS and Contributors
 # License: GNU General Public License v3. See license.txt
 
-
 import frappe
-from frappe import _
 from frappe.utils import cint
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def setup(company=None, patch=True):
 	setup_company_independent_fixtures()
-	setup_default_leaves()
 	set_accounting_journal_as_mandatory()
 	if not patch:
 		make_fixtures(company)
@@ -154,60 +151,6 @@ def default_accounts_mapping(accounts, company):
 		x: ([y.name for y in accounts if cint(y.account_number) == account_map[x]] or [""])[0]
 		for x in account_map
 	}
-
-
-def setup_default_leaves():
-	leave_types = [
-		{
-			"doctype": "Leave Type",
-			"leave_type_name": _("RTT"),
-			"name": _("RTT"),
-			"allow_encashment": 0,
-			"is_carry_forward": 0,
-			"include_holiday": 0,
-			"is_compensatory": 0,
-			"max_leaves_allowed": 0,
-		},
-		{
-			"doctype": "Leave Type",
-			"leave_type_name": _("Congés Payés"),
-			"name": _("Congés Payés"),
-			"allow_encashment": 0,
-			"is_carry_forward": 0,
-			"include_holiday": 0,
-			"is_compensatory": 0,
-			"max_leaves_allowed": 30,
-			"allow_negative": 1,
-			"is_earned_leave": 1,
-			"earned_leave_frequency": "Custom Formula",
-			"earned_leave_frequency_formula": "Congés payés sur jours ouvrables",
-		},
-		{
-			"doctype": "Leave Type",
-			"leave_type_name": _("Sick Leave"),
-			"name": _("Sick Leave"),
-			"allow_encashment": 0,
-			"is_carry_forward": 0,
-			"include_holiday": 0,
-			"is_compensatory": 0,
-			"max_leaves_allowed": 0,
-			"allow_negative": 1,
-			"is_lwp": 1,
-		},
-	]
-
-	for leave_type in leave_types:
-		doc = frappe.get_doc(leave_type)
-		doc.insert(ignore_permissions=True, ignore_if_duplicate=True)
-
-	policy = {
-		"doctype": "Leave Policy",
-		"title": _("Congés Payés"),
-		"leave_policy_details": [{"leave_type": _("Congés Payés"), "annual_allocation": 30}],
-	}
-
-	doc = frappe.get_doc(policy)
-	doc.insert(ignore_permissions=True, ignore_if_duplicate=True)
 
 
 def set_accounting_journal_as_mandatory():
