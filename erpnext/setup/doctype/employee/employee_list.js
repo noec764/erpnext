b63b94206cbddc3cfd62ba1524765bab480880d9
Charles-Henri Decultot <chdecultot@dokos.io>
2022-09-15 16:28:34 +0200

    fix: translations

--- erpnext/setup/doctype/employee/employee_list.js
+++ erpnext/setup/doctype/employee/employee_list.js
@@ -2,7 +2,7 @@ frappe.listview_settings['Employee'] = {
 	add_fields: ["status", "branch", "department", "designation","image"],
 	filters: [["status","=", "Active"]],
 	get_indicator: function(doc) {
-		var indicator = [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
+		var indicator = [__(doc.status, null, "Employee"), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
 		indicator[1] = {"Active": "green", "Inactive": "red", "Left": "gray", "Suspended": "orange"}[doc.status];
 		return indicator;
 	}
