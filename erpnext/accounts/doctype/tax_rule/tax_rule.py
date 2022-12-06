7ca73e3f99f01631a8b0d4671b103ff3801741a8
Charles-Henri Decultot <chdecultot@dokos.io>
2022-12-06 20:41:32 +0100

    fix: dates overlapping

--- erpnext/accounts/doctype/tax_rule/tax_rule.py
+++ erpnext/accounts/doctype/tax_rule/tax_rule.py
@@ -31,7 +31,8 @@ class TaxRule(Document):
 
 	def validate(self):
 		self.validate_tax_template()
-		self.validate_from_to_dates("from_date", "to_date")
+		if self.from_date and self.to_date:
+			self.validate_from_to_dates("from_date", "to_date")
 		self.validate_filters()
 		self.validate_use_for_shopping_cart()
 
