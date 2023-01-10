4e1974faba3b33df5e4caa7b1cb6345777c06611
Charles-Henri Decultot <chdecultot@dokos.io>
2023-01-10 18:05:20 +0100

    fix: Opening stock error

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -166,9 +166,6 @@ class Item(Document):
 			return
 
 		if not self.valuation_rate and not self.standard_rate and not self.is_customer_provided_item:
-			self.valuation_rate = self.standard_rate
-
-		if not self.valuation_rate and not self.is_customer_provided_item:
 			frappe.throw(_("Valuation Rate is mandatory if Opening Stock entered"))
 
 		from erpnext.stock.doctype.stock_entry.stock_entry_utils import make_stock_entry
