336ae7840b6ecc036d8271c21873b8dd9b1b6cc4
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-01-11 14:49:50 +0000

    feat(multi-company): Validate cart items

--- erpnext/selling/doctype/quotation/quotation.py
+++ erpnext/selling/doctype/quotation/quotation.py
@@ -28,6 +28,7 @@ class Quotation(SellingController):
 		self.validate_uom_is_integer("stock_uom", "qty")
 		self.validate_valid_till()
 		self.validate_shopping_cart_items()
+		self.validate_multicompany_items()
 		self.set_customer_name()
 		if self.items:
 			self.with_items = 1
@@ -35,6 +36,26 @@ class Quotation(SellingController):
 		from erpnext.stock.doctype.packed_item.packed_item import make_packing_list
 
 		make_packing_list(self)
+
+	def validate_multicompany_items(self):
+		if self.order_type != "Shopping Cart":
+			return
+
+		venue_settings = frappe.get_cached_doc("Venue Settings")
+		if venue_settings.enable_multi_companies:
+			if filter_mc := venue_settings.multicompany_get_item_filter_for_company(self.company):
+				kept_items = []
+				for item in self.items:
+					has_compatible_web_item = frappe.get_all("Website Item", limit=1, filters=[
+						["item_code", "=", item.item_code],
+						filter_mc,
+					])
+					if has_compatible_web_item:
+						kept_items.append(item)
+						continue
+				self.items = kept_items
+				if not kept_items:
+					frappe.throw(_("The shopping cart has been emptied because none of the items are available in the selected company."), frappe.MandatoryError)
 
 	def validate_valid_till(self):
 		if self.valid_till and getdate(self.valid_till) < getdate(self.transaction_date):
