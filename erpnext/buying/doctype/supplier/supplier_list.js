94f5a12a9832e7104ad47d2f9a2d13a75105a864
Charles-Henri Decultot <chdecultot@dokos.io>
2023-02-08 11:05:33 +0100

    fix: Status translation in supplier list

--- erpnext/buying/doctype/supplier/supplier_list.js
+++ erpnext/buying/doctype/supplier/supplier_list.js
@@ -2,7 +2,7 @@ frappe.listview_settings['Supplier'] = {
 	add_fields: ["supplier_name", "supplier_group", "image", "on_hold"],
 	get_indicator: function(doc) {
 		if(cint(doc.on_hold)) {
-			return [__("On Hold"), "red"];
+			return [__("On Hold", null, "Supplier"), "red"];
 		}
 	}
 };
