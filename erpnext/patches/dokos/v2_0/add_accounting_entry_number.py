8d504898f5ce2f9f94ae4a4af0ecbdbb30f08b64
Charles-Henri Decultot <chdecultot@dokos.io>
2023-02-28 20:58:18 +0100

    fix: Add accounting entry numbers for reversals

--- erpnext/patches/dokos/v2_0/add_accounting_entry_number.py
+++ erpnext/patches/dokos/v2_0/add_accounting_entry_number.py
@@ -1,10 +1,17 @@
 import frappe
+
 from erpnext.accounts.general_ledger import get_accounting_number
+
 
 def execute():
 	frappe.reload_doc("accounts", "doctype", "GL Entry")
 	vouchers = {}
-	for gl in frappe.get_all("GL Entry", order_by="posting_date, voucher_no asc", fields=["posting_date", "voucher_no", "name", "fiscal_year"]):
+	for gl in frappe.get_all(
+		"GL Entry",
+		filters={"accounting_entry_number": ("is", "not set")},
+		order_by="posting_date, voucher_no asc",
+		fields=["posting_date", "voucher_no", "name", "fiscal_year"],
+	):
 		if gl.voucher_no not in vouchers:
 			vouchers[gl.voucher_no] = get_accounting_number(gl)
 		frappe.db.set_value("GL Entry", gl.name, "accounting_entry_number", vouchers[gl.voucher_no])
