ed5f137eb3080833a3db227b204c82cf46f4baa4
Charles-Henri Decultot <chdecultot@dokos.io>
2023-03-10 10:06:40 +0100

    fix: Invert condition for registration automatic submission

--- erpnext/venue/doctype/event_registration/event_registration.py
+++ erpnext/venue/doctype/event_registration/event_registration.py
@@ -173,7 +173,7 @@ class EventRegistration(Document):
 		self.user = frappe.session.user
 		self.set_company_from_cart_settings()
 
-		if self.flags.in_payment_webform:
+		if not self.flags.in_payment_webform:
 			# Free registration
 			self.payment_status = ""
 			self.submit()  # Automatically submit when created from a Web Form.
