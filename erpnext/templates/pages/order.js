260059b4ad81a59e0bc5871f86e88c6ad4393089
Charles-Henri Decultot <chdecultot@dokos.io>
2023-03-13 10:01:42 +0100

    fix: Loyalty payments was not working

--- erpnext/templates/pages/order.js
+++ erpnext/templates/pages/order.js
@@ -1,50 +1,35 @@
-// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
 frappe.ready(function(){
 
-	const loyalty_points_input = document.getElementById("loyalty-point-to-redeem");
-	const loyalty_points_status = document.getElementById("loyalty-points-status");
-	const currency = "{{ doc.currency }}"
-	const available_loyalty_points = "{{ available_loyalty_points }}"
+	var loyalty_points_input = document.getElementById("loyalty-point-to-redeem");
+	var loyalty_points_status = document.getElementById("loyalty-points-status");
 	if (loyalty_points_input) {
 		loyalty_points_input.onblur = apply_loyalty_points;
 	}
 
 	function apply_loyalty_points() {
-
-		if (!doc_info.grand_total) {
-			frappe.msgprint(__("Loyalty points cannot be redeemed on this document."))
-			return;
-		}
-
-		const loyalty_points = parseInt(loyalty_points_input.value);
-
-		if (loyalty_points > available_loyalty_points) {
-			frappe.msgprint(__("You cannot redeem more than {0} points.", [available_loyalty_points]))
-			return;
-		}
-
+		var loyalty_points = parseInt(loyalty_points_input.value);
 		if (loyalty_points) {
 			frappe.call({
-				method: "erpnext.accounts.doctype.loyalty_program.loyalty_program.get_redeemed_amount",
+				method: "erpnext.accounts.doctype.loyalty_program.loyalty_program.get_redeemption_factor",
 				args: {
-					customer: doc_info.customer,
-					points_redeemed: parseInt(loyalty_points_input.value),
-					grand_total: doc_info.grand_total,
-					currency: currency
+					"customer": doc_info.customer
 				},
 				callback: function(r) {
 					if (r) {
-						let message = ""
-						if (!r.message.redeem_accepted) {
-							frappe.msgprint(__("You can only redeem a maximum of {0} points in this order.", [r.message.redeemable_points]));
+						var message = ""
+						let loyalty_amount = flt(r.message*loyalty_points);
+						if (doc_info.grand_total && doc_info.grand_total < loyalty_amount) {
+							let redeemable_amount = parseInt(doc_info.grand_total/r.message);
+							message = "You can only redeem max " + redeemable_amount + " points in this order.";
+							frappe.msgprint(__(message));
 						} else {
-							loyalty_points_status.innerText = __("{0} loyalty points applied for an amount of {1}.", [loyalty_points, r.message.loyalty_amount]);
-							const payment_button = document.getElementById("pay-for-order");
-							payment_button.innerText = __("Pay {0}", [r.message.remaining_amount]);
+							message = loyalty_points + " Loyalty Points of amount "+ loyalty_amount + " is applied."
+							frappe.msgprint(__(message));
+							var payment_button = document.getElementById("pay-for-order");
+							payment_button.innerHTML = __("Pay Remaining");
 							payment_button.href = "/api/method/erpnext.accounts.doctype.payment_request.payment_request.make_payment_request?dn="+doc_info.doctype_name+"&dt="+doc_info.doctype+"&loyalty_points="+loyalty_points+"&submit_doc=1&order_type=Shopping Cart";
 						}
+						loyalty_points_status.innerHTML = message;
 					}
 				}
 			});
