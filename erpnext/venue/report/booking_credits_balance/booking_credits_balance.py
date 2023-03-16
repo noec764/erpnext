fb40cd8dd1033a8bcca25b9c94ee412aa947506f
Charles-Henri Decultot <chdecultot@dokos.io>
2023-03-16 21:25:42 +0100

    feat: Add a graph to report

--- erpnext/venue/report/booking_credits_balance/booking_credits_balance.py
+++ erpnext/venue/report/booking_credits_balance/booking_credits_balance.py
@@ -1,14 +1,21 @@
 # Copyright (c) 2023, Dokos SAS and contributors
 # For license information, please see license.txt
 
+from collections import defaultdict
+
 import frappe
 from frappe import _
+from frappe.utils import cint
 
 from erpnext.venue.doctype.booking_credit.booking_credit import get_balance
 
+
 def execute(filters=None):
-	columns, data = get_columns(), get_data(filters)
-	return columns, data
+	columns = get_columns()
+	data = get_data(filters)
+	chart = get_chart(data)
+	return columns, data, None, chart
+
 
 def get_data(filters):
 	result = []
@@ -17,11 +24,7 @@ def get_data(filters):
 	filters["status"] = "Active"
 
 	customers = frappe.get_list(
-		"Booking Credit",
-		filters=filters,
-		order_by=f"customer asc",
-		pluck="customer",
-		distinct=True
+		"Booking Credit", filters=filters, order_by="customer asc", pluck="customer", distinct=True
 	)
 
 	for customer in customers:
@@ -32,11 +35,12 @@ def get_data(filters):
 				{
 					"customer": customer if index == 0 else "",
 					"booking_credit_type": bal,
-					"balance": balance[bal]
+					"balance": balance[bal],
 				}
 			)
 
 	return result
+
 
 def get_columns():
 	return [
@@ -45,19 +49,50 @@ def get_columns():
 			"fieldtype": "Link",
 			"label": _("Customer"),
 			"options": "Customer",
-			"width": 300
+			"width": 300,
 		},
 		{
 			"fieldname": "booking_credit_type",
 			"fieldtype": "Link",
 			"label": _("Booking Credit Type"),
 			"options": "Booking Credit Type",
-			"width": 300
+			"width": 300,
 		},
-		{
-			"fieldname": "balance",
-			"fieldtype": "Int",
-			"label": _("Balance"),
-			"width": 200
-		}
+		{"fieldname": "balance", "fieldtype": "Int", "label": _("Balance"), "width": 200},
 	]
+
+
+def get_chart(data):
+	data_by_booking_credit_type = defaultdict(int)
+
+	for d in data:
+		data_by_booking_credit_type[d.get("booking_credit_type")] += cint(d.get("balance"))
+
+	chart = {
+		"data": {
+			"labels": [d for d in data_by_booking_credit_type],
+			"datasets": [
+				{
+					"name": _("Balance by credit type"),
+					"values": [data_by_booking_credit_type[d] for d in data_by_booking_credit_type],
+				}
+			],
+		},
+		"type": "percentage",
+		"colors": [
+			"#00bdff",
+			"#1b3bff",
+			"#8F00FF",
+			"#ff0011",
+			"#ff7300",
+			"#ffd600",
+			"#00c30e",
+			"#65ff00",
+			"#d200ff",
+			"#FF00FF",
+			"#7d7d7d",
+			"#5d5d5d",
+		],
+	}
+
+	return chart
