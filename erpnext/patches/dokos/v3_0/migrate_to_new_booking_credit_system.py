0aeb128d9c351973abaa725bd5f32b814f45e28c
Charles-Henri Decultot <chdecultot@dokos.io>
2023-03-17 08:56:48 +0100

    fix: Patch and workspace

--- erpnext/patches/dokos/v3_0/migrate_to_new_booking_credit_system.py
+++ erpnext/patches/dokos/v3_0/migrate_to_new_booking_credit_system.py
@@ -3,16 +3,29 @@ from collections import defaultdict
 import frappe
 from frappe.utils import cint
 
+
 def execute():
 	conversions = frappe.get_all("Booking Credit Conversion")
 	booking_types = defaultdict(dict)
 
 	for conversion in conversions:
 		conversion_doc = frappe.get_doc("Booking Credit Conversion", conversion.name)
-		for rule in frappe.get_all("Booking Credit Rule", filters={"rule_type": "Booking Credits Addition", "applicable_for": "Item", "applicable_for_document_type": conversion_doc.booking_credits_item}):
+		for rule in frappe.get_all(
+			"Booking Credit Rule",
+			filters={
+				"rule_type": "Booking Credits Addition",
+				"applicable_for": "Item",
+				"applicable_for_document_type": conversion_doc.booking_credits_item,
+			},
+		):
 			rule = frappe.get_doc("Booking Credit Rule", rule.name)
 
-			for credits in frappe.get_all("Booking Credit", filters={"item": conversion_doc.booking_credits_item}, fields=["uom"], distinct=True):
+			for credits in frappe.get_all(
+				"Booking Credit",
+				filters={"item": conversion_doc.booking_credits_item},
+				fields=["uom"],
+				distinct=True,
+			):
 				booking_type = frappe.new_doc("Booking Credit Type")
 				if frappe.db.exists("Booking Credit Type", dict(item=conversion_doc.booking_credits_item)):
 					booking_type.label = f"{conversion_doc.booking_credits_item}-{credits.uom}"
@@ -22,10 +35,7 @@ def execute():
 				booking_type.uom = credits.uom
 				booking_type.validity = get_validity(rule)
 				for con in conversion_doc.conversion_table:
-					booking_type.append("conversion_table", {
-						"item": con.convertible_item,
-						"credits": 1
-					})
+					booking_type.append("conversion_table", {"item": con.convertible_item, "credits": 1})
 
 				booking_type.insert()
 
@@ -35,13 +45,27 @@ def execute():
 		for booking_credit in frappe.get_all(dt, fields=["name", "item", "uom"]):
 			bt = booking_types.get(booking_credit.item, {}).get(booking_credit.uom, {})
 			if not bt:
-				print(f"No booking credit type found for item {booking_credit.item} and uom {booking_credit.uom}")
+				print(
+					f"No booking credit type found for item {booking_credit.item} and uom {booking_credit.uom}"
+				)
 				continue
 
 			frappe.db.set_value(dt, booking_credit.name, "booking_credit_type", bt)
 
-	for reference in frappe.get_all("Booking Credit Usage Reference", filters={"reference_doctype": "Item Booking"}, fields=["reference_document", "booking_credit_usage"]):
-		frappe.db.set_value("Booking Credit Usage", reference.booking_credit_usage, "item_booking", reference.reference_document)
+	for reference in frappe.get_all(
+		"Booking Credit Usage Reference",
+		filters={"reference_doctype": "Item Booking"},
+		fields=["reference_document", "booking_credit_usage"],
+	):
+		frappe.db.set_value(
+			"Booking Credit Usage",
+			reference.booking_credit_usage,
+			"item_booking",
+			reference.reference_document,
+		)
+
+	for pr in frappe.get_all("Pricing Rule", filters={"booking_credits_based": 1}):
+		frappe.delete_doc("Pricing Rule", pr.name, force=True)
 
 
 def get_validity(rule):
