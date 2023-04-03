1bb3ce48a1a8d8ca6f64b14b378550f8687d48c3
Charles-Henri Decultot <chdecultot@dokos.io>
2023-04-03 07:47:29 +0200

    fix: Multiple issues in purchase invoice submission

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -157,7 +157,7 @@ class SalesInvoice(SellingController):
 
 		self.set_against_income_account()
 		self.validate_time_sheets_are_submitted()
-		self.validate_multiple_billing("Delivery Note", "dn_detail", "amount", "items")
+		self.validate_multiple_billing("Delivery Note", "dn_detail", "amount")
 		if not self.is_return:
 			self.validate_serial_numbers()
 		else:
